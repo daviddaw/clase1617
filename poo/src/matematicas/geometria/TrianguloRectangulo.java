08273c9abbf0737d95dcba30b5842818477b120e
david <david.palanco.daw@gmail.com>
2017-01-10 14:05:36 +0100

    metodo getHipotenusa()

--- poo/src/matematicas/geometria/TrianguloRectangulo.java
+++ poo/src/matematicas/geometria/TrianguloRectangulo.java
@@ -1,6 +1,33 @@
 package matematicas.geometria;
 
 public class TrianguloRectangulo {
-private int cateto1;
-private int cateto2;
+private double cateto1;
+private double cateto2;
+
+public TrianguloRectangulo(double cateto1, double cateto2) {
+	this.cateto1 = cateto1;
+	this.cateto2 = cateto2;
+}
+
+public double getCateto1() {
+	return cateto1;
+}
+
+public void setCateto1(double cateto1) {
+	this.cateto1 = cateto1;
+}
+
+public double getCateto2() {
+	return cateto2;
+}
+
+public void setCateto2(double cateto2) {
+	this.cateto2 = cateto2;
+}
+public double getHipotenusa() {
+	return Math.sqrt(Math.pow(cateto1, 2)+Math.pow(cateto2, 2));
+	
+}
+
+
 }
