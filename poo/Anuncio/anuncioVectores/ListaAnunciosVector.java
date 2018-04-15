2334fa63a05aeaad0258e9a861cd827c6f12eb0e
Daveto <David.palanco@gmail.com>
2018-04-15 21:05:11 +0200

    actualizcion de metodoy he tocadoalgo el main

--- poo/Anuncio/anuncioVectores/ListaAnunciosVector.java
+++ poo/Anuncio/anuncioVectores/ListaAnunciosVector.java
@@ -33,4 +33,18 @@ public  class ListaAnunciosVector {
     public int cantidadAnuncios(){
             return listaAnuncios.size();
     }
+    public double sumaTiempoEmisora(String Emisora) {
+    	double tiempoEmisora = 0;
+    	AnuncioRadioTV auxE = null ;
+    	for (int i = 0; i < listaAnuncios.size(); i++) {
+			if (listaAnuncios.get(i) instanceof AnuncioRadioTV) {
+				auxE=(AnuncioRadioTV)listaAnuncios.get(i);
+			}
+			if (auxE.getEmisora().equals(Emisora)) {
+				tiempoEmisora+=auxE.getDuracion();
+			}
+		}
+    	
+		return tiempoEmisora;
+    }
 }
